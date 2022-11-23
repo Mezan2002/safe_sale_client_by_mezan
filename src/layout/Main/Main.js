9bd057dd72ac4d14866f2e55cf882e65e8483de4
Mezan2002 <mezanurr009@gmail.com>
2022-11-24 00:53:35 +0600

    Footer added

--- src/layout/Main/Main.js
+++ src/layout/Main/Main.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../../pages/shared/Footer/Footer";
 import Navbar from "../../pages/shared/Navbar/Navbar";
 
 const Main = () => {
   return (
     <div>
-      <Navbar></Navbar>
-      <Outlet></Outlet>
+      <div className="max-w-screen-2xl mx-auto min-h-[90vh]">
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
