4cb2e40e6c4009e57f141cce374ec1ea64843c7e
Mezan2002 <mezanurr009@gmail.com>
2022-11-26 22:26:45 +0600

    react query used

--- src/layout/Dashboard/MyProducts/MyProducts.js
+++ src/layout/Dashboard/MyProducts/MyProducts.js
@@ -1,9 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../contexts/AuthProvider/AuthProvider";
 
 const MyProducts = () => {
   const { user } = useContext(AuthContext);
   const [myProducts, setMyProducts] = useState([]);
+  const { data: productsData = [] } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:5000/products?email=${user?.email}`
+      );
+      const data = res.json();
+      return data;
+    },
+  });
   useEffect(() => {
     fetch(`http://localhost:5000/products?email=${user?.email}`)
       .then((res) => res.json())
@@ -12,6 +23,19 @@ const MyProducts = () => {
         console.log(data);
       });
   }, [user?.email]);
+
+  const handleAdvertise = (id) => {
+    fetch(`http://localhost:5000/products/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
 
   return (
     <div>
@@ -36,7 +60,10 @@ const MyProducts = () => {
                 <td>{product.resalePrice}</td>
                 <td>{product.status}</td>
                 <td>
-                  <button className="btn btn-sm btn-success text-white">
+                  <button
+                    onClick={() => handleAdvertise(product._id)}
+                    className="btn btn-sm btn-success text-white"
+                  >
                     Advertise
                   </button>
                 </td>
