77d32fb6de353c320d301790c73b508d38581fe3
Mezan2002 <mezanurr009@gmail.com>
2022-11-28 03:32:11 +0600

    Send report for a product

--- src/pages/Home/Categories/SingleProductCard/SingleProductCard.js
+++ src/pages/Home/Categories/SingleProductCard/SingleProductCard.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
-import { FaCheckCircle } from "react-icons/fa";
+import { FaBookmark, FaCheckCircle, FaExclamationCircle } from "react-icons/fa";
 import Loading from "../../../shared/Loading/Loading";
+import toast from "react-hot-toast";
 
-const SingleProductCard = ({ product, setProduct, setRefetch }) => {
+const SingleProductCard = ({ product, setProduct }) => {
   const {
     productPhoto,
     name,
@@ -17,6 +18,7 @@ const SingleProductCard = ({ product, setProduct, setRefetch }) => {
     sellerPhoneNumber,
     productAddedDate,
     status,
+    _id,
   } = product;
 
   const { data: user = [], isLoading } = useQuery({
@@ -28,6 +30,24 @@ const SingleProductCard = ({ product, setProduct, setRefetch }) => {
     },
   });
 
+  const handleReport = (id) => {
+    const proceed = window.confirm(
+      "Are you really want to report this product?"
+    );
+    if (proceed) {
+      fetch(`http://localhost:5000/products/reported/${id}`, {
+        method: "PATCH",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.modifiedCount === 1) {
+            toast.success("Product Reported Successfully");
+          }
+          console.log(data);
+        });
+    }
+  };
+
   if (isLoading) {
     return <Loading></Loading>;
   }
@@ -37,17 +57,36 @@ const SingleProductCard = ({ product, setProduct, setRefetch }) => {
       <div className="card lg:card-side shadow-xl w-8/12 mx-auto mb-20">
         <img src={productPhoto} className="h-96 mt-7 ml-5" alt="" />
         <div className="card-body">
-          <h2 className="text-4xl font-bold text-center md:text-left">
-            {name}
-          </h2>
-          <h4 className="flex items-center justify-center md:justify-start font-bold mb-7 text-center md:text-left">
-            Posted By {sellerName}
-            {user[0].isVerified === true ? (
-              <span className="ml-2">
-                <FaCheckCircle className="text-green-500"></FaCheckCircle>
-              </span>
-            ) : null}
-          </h4>
+          <div className="flex justify-between items-start">
+            <div>
+              <h2 className="text-4xl font-bold text-center md:text-left">
+                {name}
+              </h2>
+              <h4 className="flex items-center justify-center md:justify-start font-bold mb-7 text-center md:text-left">
+                Posted By {sellerName}
+                {user[0].isVerified === true ? (
+                  <span className="ml-2">
+                    <FaCheckCircle className="text-green-500"></FaCheckCircle>
+                  </span>
+                ) : null}
+              </h4>
+            </div>
+            <div className="flex items-center">
+              <button>
+                <FaBookmark
+                  className="text-2xl mr-2 mt-4"
+                  title="Save Product"
+                ></FaBookmark>
+              </button>
+              <button>
+                <FaExclamationCircle
+                  onClick={() => handleReport(_id)}
+                  className="text-2xl mt-4"
+                  title="Report Product"
+                ></FaExclamationCircle>
+              </button>
+            </div>
+          </div>
           <div className="font-semibold mb-5 text-center md:text-left">
             <p>Original Price: {originalPrice} BDT</p>
             <p>Resale Price: {resalePrice} BDT</p>
